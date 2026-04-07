7c5320c82b961e47c0508b52a6296ca553b0dfa4
CarlosSequre <cvasquez@sequrequantum.com>
2026-04-06 21:02:44 -0400

    avance de clase al 06 abril

---
+++ src/app/product/product-list/product-list.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+import { IProduct } from '../../product';
+
+@Component({
+  selector: 'app-product-list',
+  imports:[],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css',
+})
+export class ProductList {
+  products = input<IProduct[]>([], { alias: 'datos'});
+  imageWidth: number = 50;
+  imageHeight: number = 50;
+  imageMargin: number = 10;
+  showImage: boolean =false;
+
+  toggleImage():void {
+    this.showImage = !this.showImage;
+  }
+}
